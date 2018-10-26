722de7182649b32561bdbc9dfcae25d0339f1233
Doug Ferris <dferris@geomatic.com.au>
2018-10-26 23:26:06 +1100

    Changes made during Friday of PAX Aus

--- helm/csgo-comp/start.sh
+++ helm/csgo-comp/start.sh
@@ -2,5 +2,6 @@
 for i in 1 2 3 4 5 6 7 8 
 do
 echo server_num: $i > server_num.yaml
-helm install csgo-wingman-$i -f  server_num.yaml -f wingman.yaml .
+helm install --name csgocomp-$i -f  server_num.yaml .
+#helm delete --purge csgocomp-$i
 done
--- helm/factorio/start.sh
+++ helm/factorio/start.sh
@@ -3,8 +3,8 @@ for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
 
 do
 echo server_num: $i > server_num.yaml
-helm install --name factorio$i -f server_num.yaml .
-#helm del --purge factorio$i
+#helm install --name factorio$i -f server_num.yaml .
+helm del --purge factorio$i
 #helm upgrade factorio$i -f server_num.yaml .
 done
 
